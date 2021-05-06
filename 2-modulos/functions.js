c0af34bd3011238fbc4ca740767ebe286ca36cd8
David Copano <davidcopano96@gmail.com>
2021-05-06 18:47:25 +0200

    functions.js
    functions.js

--- 2-modulos/functions.js
+++ 2-modulos/functions.js
@@ -9,3 +9,11 @@ function substract(x1, x2) {
 function multiply(x1, x2) {
   return x1 * x2;
 }
+
+function divide(x1, x2) {
+  if(x2 === 0) {
+    console.log('No es posible dividir entre 0');
+    return;
+  }
+  return x1 / x2;
+}
