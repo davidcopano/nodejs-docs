790d2db27ae8b6514524c0e5b5976ca0ca561b85
David Copano <davidcopano96@gmail.com>
2021-05-07 14:40:55 +0200

    Renombrado archivo de modulo de ejemplo

--- 2-modulos/index.js
+++ 2-modulos/index.js
@@ -1,10 +1,10 @@
-const functions = require('./functions.js');
+const exampleModule = require('./example-module.js');
 
-console.log('Suma: ', functions.math.add(9, 5));
-console.log('Resta: ', functions.math.substract(6, 8));
-console.log('Multiplicacion: ', functions.math.multiply(4, 7));
-console.log('Division: ', functions.math.divide(6, 3));
+console.log('Suma: ', exampleModule.math.add(9, 5));
+console.log('Resta: ', exampleModule.math.substract(6, 8));
+console.log('Multiplicacion: ', exampleModule.math.multiply(4, 7));
+console.log('Division: ', exampleModule.math.divide(6, 3));
 
-functions.hello('David');
+exampleModule.hello('David');
 
-console.log(functions.variablePrueba);
+console.log(exampleModule.variablePrueba);
