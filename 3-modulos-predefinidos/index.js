4f778bbd2511a6a6084eff51c9bd637572d26f4f
David Copano <davidcopano96@gmail.com>
2021-05-09 13:28:44 +0200

    3 modulos pred.

--- 3-modulos-predefinidos/index.js
+++ 3-modulos-predefinidos/index.js
@@ -1,6 +1,18 @@
 const os = require('os');
+const fs = require('fs');
 
 console.log(os.platform());
 console.log(os.release());
-console.log('freemem:', os.freemem());
-console.log('totalmem:', os.totalmem());
+console.log('freemem:', os.freemem(), 'bytes');
+console.log('totalmem:', os.totalmem(), 'bytes');
+
+console.log('---------------------');
+
+fs.writeFile('./texto.txt', 'linea uno', (err) => {
+  if(err) {
+    console.log(err);
+  }
+  console.log('Archivo creado');
+});
+
+console.log('ultima linea de codigo');
