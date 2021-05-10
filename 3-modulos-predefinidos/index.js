81a898b3a1094fc782398852a12fad1f30202ab2
david copano <davidcopano96@gmail.com>
2021-05-10 17:39:13 +0000

    3 mod. pred

--- 3-modulos-predefinidos/index.js
+++ 3-modulos-predefinidos/index.js
@@ -1,7 +1,7 @@
 const os = require('os');
 const fs = require('fs');
 
-console.log(os.platform());
+/* console.log(os.platform());
 console.log(os.release());
 console.log('freemem:', os.freemem(), 'bytes');
 console.log('totalmem:', os.totalmem(), 'bytes');
@@ -16,3 +16,12 @@ fs.writeFile('./texto.txt', 'linea uno', (err) => {
 });
 
 console.log('ultima linea de codigo');
+
+console.log('---------------'); */ 
+
+fs.readFile('./texto.txt', (err, data) => {
+  if(err) {
+    console.log(err);
+  }
+  console.log('Contenido archivo:', data.toString());
+}); 
