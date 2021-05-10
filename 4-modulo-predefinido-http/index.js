7d5637737ee861da4b4e347619c5910c7f467a8f
david copano <davidcopano96@gmail.com>
2021-05-10 18:03:11 +0000

    4 modulo http

--- 4-modulo-predefinido-http/index.js
+++ 4-modulo-predefinido-http/index.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+  
+  console.log('request received:', req);
+ 
   res.write('<h1>Hola mundo desde Nodejs</h1>');
   res.end();  
-}).listen(3000);
+}).listen(3000, 'localhost', () => {
+  console.log('listening');
+});
 
 
